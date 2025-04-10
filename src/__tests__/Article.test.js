020364d5531b35b926f2be7bd43db7202755fe40
cjchichi <cynthia1973chichi@gmail.com>
2025-04-10 13:07:44 +0300

    add   src/__tests__/Article.test.js         modified:   src/components/Article.js         modified:   src/components/Header.js

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -5,7 +5,10 @@ import React from "react";
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
-  render(<Article />);
+  render(<Article/> );
+
+  const element = screen.queryByText("please pass this test");
+  screen.debug();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
